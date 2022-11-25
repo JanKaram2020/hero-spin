c166a14f02a99a40665c8cf5bd0d56c5ef5633ee
Jan Karam <65977048+JanKaram2020@users.noreply.github.com>
2022-11-25 19:32:22 +0200

    add select hero, random hero and random hero with name pages.

---
+++ pages/random-hero/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Index = () => <div>Random Hero</div>;
+
+export default Index;
---
+++ pages/select-hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SelectHero = () => (
+  <div>
+    <h1>Select hero</h1>
+  </div>
+);
+
+export default SelectHero;
